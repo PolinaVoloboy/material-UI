bbc56467b0b973f2783aebe7ac1f770060cd11c5
PolinaV <polivoloboy@gmail.com>
2021-06-14 17:06:51 +0200

    added text fields

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
-import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined'
+import { makeStyles, TextField } from '@material-ui/core'
+
+const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'block'
+  }
+})
 
 export default function Create() {
+  const classes = useStyles()
+
+  const [title, setTitle] = useState('');
+  const [details, setDetails] = useState('');
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
+
+    if (title == ''){
+      setTitleError(true);
+    }
+    if (details == ''){
+      setDetailsError(true);
+    }
+    if (title && details) {
+      console.log(title, details)
+    }
+  }
+
   return (
     <Container size="sm">
+     
       <Typography
         variant="h6" 
         color="textSecondary"
@@ -16,26 +49,35 @@ export default function Create() {
       >
         Create a New Note
       </Typography>
-
-      <Button
-        onClick={() => console.log('you clicked me')}
-        type="submit" 
-        color="secondary" 
-        variant="contained"
-        // startIcon={<SendIcon />}
-        endIcon={<KeyboardArrowRightIcon />}>
-        Submit
-      </Button>
-
-      {/* icons */}
-      <br/>
-      <AcUnitOutlinedIcon />
-      <AcUnitOutlinedIcon color="secondary" fontSize="large" />
-      <AcUnitOutlinedIcon color="secondary" fontSize="small" />
-      <AcUnitOutlinedIcon color="action" fontSize="small" />
-      <AcUnitOutlinedIcon color="error" fontSize="small" />
-      <AcUnitOutlinedIcon color="disabled" fontSize="small" />
-      
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}> 
+        <TextField className={classes.field}
+          onChange={(e) => setTitle(e.target.value)}
+          label="Note Title"
+          variant="outlined"
+          color="secondary"
+          required
+          fullWidth
+          error={titleError}
+        />
+        <TextField className={classes.field}
+          onChange={(e) => setDetails(e.target.value)}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          required
+          fullWidth
+          multiline
+          rows = {4}
+          error={detailsError}
+        />
+        <Button
+          type="submit" 
+          color="secondary" 
+          variant="contained"
+          endIcon={<KeyboardArrowRightIcon />}>
+          Submit
+        </Button>
+      </form> 
     </Container>
   )
 }
